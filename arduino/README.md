62c59cd283594cbf4ea300d6ace3e0b205bfa023
mekkkwiz <themakza0021@gmail.com>
2023-03-07 23:17:15 +0700

    add aruino file

--- arduino/README.md
+++ arduino/README.md
@@ -1,2 +1,25 @@
-# Arduino
+# Smart Bin Arduino Code
 
+This is the Arduino code for a smart bin that measures the distance using an ultrasonic sensor and sends a request to an API when the bin is full.
+
+## Setup
+
+To use this code, you will need an ESP12-F microcontroller, an ultrasonic sensor, and an LED.
+
+You will also need to connect your ESP12-F to the internet by providing your WiFi SSID and password.
+
+Make sure to install the `ESP8266HTTPClient.h`, `WiFiClient.h` and `ESP8266WiFi.h` libraries before uploading the code to the ESP12-F.
+
+## Usage
+
+The ultrasonic sensor measures the distance from the top of the bin to the trash inside. When the distance is less than 50 centimeters, the bin is considered full and the LED will turn red. At the same time, the code will send a POST request to the API with the bin ID, location, and status set to "full". After a 5 second delay, the LED will turn off.
+
+If the distance is more than 50 centimeters, the bin is considered not full and the LED will remain off. The code will send a POST request to the API with the bin ID, location, and status set to "not full". After a 10 second delay, the process will repeat.
+
+## Credits
+
+This code was developed by mekkkwiz for Smart Bin Arduino Code
+
+## License
+
+This code is released under the [MIT License](https://opensource.org/license/mit/).
---
+++ arduino/binNode/binNode.ino
@@ -0,0 +1,104 @@
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+
+const char* ssid = "308_MAIFREE_WIFI";
+const char* password = "beer2543";
+const char* serverUrl = "http://722b-202-44-215-20.ap.ngrok.io/api/bins";
+
+#define TRIG_PIN 4
+#define ECHO_PIN 2
+#define LED_PIN BUILTIN_LED
+#define RESISTOR_PIN A0
+
+long duration;
+int distance;
+String previousStatus = "";
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(2000);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi is connected");
+}
+
+void loop() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // for real untrasonic
+  // duration = pulseIn(ECHO_PIN, HIGH);
+  // distance = duration * 0.034 / 2;
+
+  // untrasonic simulator with variable resistor
+  int resistance = analogRead(RESISTOR_PIN);
+  int distance = map(resistance, 0, 1023, 0, 500);
+
+
+
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  String currentStatus;
+  if (distance < 50) {            // if bin is full
+    digitalWrite(LED_PIN, HIGH);  // turn LED to red
+    currentStatus = "full";
+    delay(10000);                 // wait for 10 seconds
+  } else {
+    digitalWrite(LED_PIN, LOW);   // turn LED off
+    currentStatus = "not full";
+    delay(10000);                 // wait for 10 seconds
+  }
+
+  if (currentStatus != previousStatus) {
+    sendRequest(currentStatus);
+    previousStatus = currentStatus;
+  }
+}
+
+void sendRequest(String status)
+{
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    HTTPClient http;
+    WiFiClient client;
+
+    http.begin(client, serverUrl);
+    http.addHeader("Content-Type", "application/json");
+
+    const String binId = "bin-02";
+    const String binLocation = "{\"lat\":18.7954436,\"lng\":98.9513678}";
+
+    String payload = "{\"id\":\"" + binId + "\",\"location\":" + binLocation + ",\"status\":\"" + status + "\"}";
+    Serial.print("Payload: ");
+    Serial.println(payload);
+    Serial.print("POST payload to URL: ");
+    Serial.println(serverUrl);
+    int httpCode = http.POST(payload);
+    if (httpCode > 0)
+    {
+      String response = http.getString();
+      Serial.print("Response: ");
+      Serial.println(response);
+    }
+    else
+    {
+      Serial.print("httpCode: ");
+      Serial.println(httpCode);
+      Serial.println("Error sending request");
+    }
+
+    http.end();
+  }
+}
